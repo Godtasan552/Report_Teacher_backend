348c92ec14051bc6bfc40e12c59f69b908a68a6c
Godtasan552 <werawat7903@gmail.com>
2025-09-07 06:05:07 +0700

    create admin and create function generate tracking_id

--- Admin_Report/admin.py
+++ Admin_Report/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Admin_Report
+
+@admin.register(Admin_Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ["tracking_id", "teacher", "subject", "status", "created_at"]
+    list_filter = ["status", "created_at"]
+    search_fields = ["tracking_id", "teacher", "subject"]
 
 # Register your models here.
--- Admin_Report/views.py
+++ Admin_Report/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets,permissions
+from rest_framework.response import Response
+from .models import Admin_Report
+from .serializers import ReportSerializer
+import random, string
 
-# Create your views here.
+# สร้าง tracking_id แบบสุ่ม
+def generate_tracking_id():
+    return '#' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+class ReportViewSet(viewsets.ModelViewSet):
+    queryset = Admin_Report.object.all()
+    serializer_class = ReportSerializer
+    permission_classes = [permissions.AllowAny]  # อนุญาตให้ทุกคนเข้าถึง API นี้ได้
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data["tracking_id"] = generate_tracking_id()
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({"tracking_id": serializer.data["tracking_id"]})
+
+    def retrieve(self, request, *args, **kwargs):
+        report = self.get_object()
+        return Response({
+            "tracking_id": report.tracking_id,
+            "status": report.status,
+            "response": report.response
+        })
